26a433f792092dcb4bd72d90d29b0932238d2685
Soma Bera <somabera6201688@gmail.com>
2026-06-06 14:17:20 +0530

    third deploy

--- controller/productcontroller.js
+++ controller/productcontroller.js
@@ -65,3 +65,13 @@ exports.deleteproduct=async (req,res)=>
         res.json({"msg":error.message})
     }
 }
+
+exports.bulkinsert=async(req,res)=>{
+  try{
+    await products.insertMany(req.body)
+    res.json({"msg":"products saved"})
+  }
+  catch(error){
+    res.json(error.message)
+  }
+};
--- routes/productroutes.js
+++ routes/productroutes.js
@@ -1,12 +1,14 @@
 const { Router } = require('express');
-const { createproduct, getproduct, updateproduct, deleteproduct } = require('../controller/productcontroller');
+const { createproduct, getproduct, updateproduct, deleteproduct , bulkinsert} = require('../controller/productcontroller');
 
 const router = Router();
 
 router.post('/', createproduct);
+router.post('/bulk',bulkinsert);
 router.get('/', getproduct);
 router.put('/:id', updateproduct);
 router.delete('/:id', deleteproduct);
 
 module.exports = router;
 //exports.router = router;
+
